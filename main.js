5784af60b6718f8c6ab96e1cbce87baffbb14789
aakash kumar <akparjapati2022@gmail.com>
2025-05-01 12:52:42 +0530

    add ScrollToView Feature

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ hamburger.addEventListener("click",()=>{
 hamburger.classList.toggle("fa-x");
 list.classList.toggle("navlist-active");
 })
+
+let contact = document.querySelector("#contact")
+
+contact.addEventListener("click",()=>{
+document.querySelector(".input-container").scrollIntoView({behavior:"smooth"})
+})
